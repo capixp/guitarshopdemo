8b3b3036f321c284dd8074863f07081a299d7d07
capixp <warnerxp@gmail.com>
2025-09-16 19:20:27 -0600

    Custom Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,53 @@
-import Header from "./components/Header"
-import Guitar from "./components/Guitar"
-import { useEffect, useState } from "react"
-import { db } from "./data/db"
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { useEffect, useState } from "react";
+import { db } from "./data/db";
+import { useCart } from "./hooks/useCart";
 function App() {
- 
-    const initialCart = () =>{
-      const localStorageCart = localStorage.getItem('cart')
-      return localStorageCart ? JSON.parse(localStorageCart): []
-    }
-    const [data] = useState(db)
-    const [cart, setCart] = useState(initialCart)
-
-    const MAX_ITEMS = 5
-    const MIN_ITEMS = 1
-
-    useEffect(()=>{
-      localStorage.setItem('cart',JSON.stringify(cart))
-    },[cart])
-    
-    function addToCart(item){
-      const itemExists = cart.findIndex((guitar)=>guitar.id === item.id)
-      if(itemExists >= 0)
-      {
-        if(cart[itemExists].quantity >= MAX_ITEMS) return
-        const updateCart = [...cart]
-        updateCart[itemExists].quantity++
-        setCart(updateCart)
-        console.log('Existe')
-      }
-      else
-      {
-        item.quantity = 1
-        console.log('No Existe')
-        setCart([...cart,item])
-      }
-    }
-    
-    function removeFromCart(id){
-      setCart(prevCart=>prevCart.filter(guitar => guitar.id !== id))
-    }
-
-    function increaseQuantity(id)
-    {
-      const updateCart = cart.map(item=>{
-        if(item.id === id && item.quantity < MAX_ITEMS)
-        {
-          return{
-            ...item,
-            quantity: item.quantity +1
-          }
-            
-        }
-        return item
-      })
-      setCart(updateCart)
-    }
-
-    function decreaseQuantity(id){
-      const updateCartAfterDecrease = cart.map(item =>{
-        if(item.id === id && item.quantity > MIN_ITEMS)
-        {
-          return{
-            ...item,
-            quantity: item.quantity - 1
-          }
-        }
-        return item
-      })
-      setCart(updateCartAfterDecrease)
-    }
-
-    function clearCart(){
-      setCart([])
-    }
+  const {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal,
+    totalItems,
+  } = useCart();
 
   return (
     <>
-    <Header
-    cart = {cart}
-    removeFromCart = {removeFromCart}
-    increaseQuantity = {increaseQuantity}
-    decreaseQuantity = {decreaseQuantity}
-    clearCart = {clearCart}
-    />
-    <main className="container-xl mt-5">
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+        isEmpty={isEmpty}
+        cartTotal={cartTotal}
+        totalItems={totalItems}
+      />
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map((guitar)=>
-             (
-                <Guitar
-                key = {guitar.id}
-                guitar = {guitar}
-                setCart = {setCart}
-                addToCart = {addToCart}
-                
-                />
-             ))}
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
